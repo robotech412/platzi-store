a8b801150d6c4c5c0451e78cf5068eec6ef1f91a
Dennis Ake <dariel_504@hotmail.com>
2021-02-26 21:01:14 -0600

    agregado ejercicio del ciclo de  vida de los componentes en angular

--- src/app/components/product.component.ts
+++ src/app/components/product.component.ts
@@ -1,17 +1,41 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter, OnChanges, SimpleChanges, OnInit, DoCheck } from '@angular/core';
 
 import { Product } from '../product.model';
 
+// tslint:disable-next-line: no-conflicting-lifecycle
 @Component({
     selector: 'app-product',
     templateUrl: './product.component.html'
 })
-export class ProductComponent {
+export class ProductComponent implements DoCheck {
 
     @Input() product: Product;
     @Output() productClicked: EventEmitter<any> = new EventEmitter(); //any emitir cualquier valor
 
-    // tslint:disable-next-line: typedef
+    constructor() {
+        console.log('1. constructor');
+    }
+
+
+    //ngOnChanges(changes: SimpleChanges): void {
+     //   console.log('2. ngOnChanges');
+      //  console.log(changes);
+  //  }
+
+
+    ngOnInit(): void {
+        console.log('3. ngOnInit');
+    }
+
+    ngDoCheck(){
+        console.log('4. ngDocheck');
+
+    }
+
+ngOnDestroy(){
+    console.log('5. ngOnDestroy');
+}
+
     addCart() {
         console.log('añadir al carrito');
         this.productClicked.emit(this.product.id);
