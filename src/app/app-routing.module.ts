6a4f7070d1d3395baf2193ebbddfa8e35e29b6c3
Dennis Ake <dariel_504@hotmail.com>
2021-03-05 16:53:26 -0600

    modulo del website con vistas anidadas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,28 +7,31 @@ import { ProductsComponent } from './products/products.component';
 import { DemoComponent } from './demo/demo.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { ProductDetailComponent } from './product-detail/product-detail.component';
-
+import { LayoutComponent } from './layout/layout.component';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: '/home',
-    pathMatch: 'full',
-  },
-  {
-    path: 'home',
-    component: HomeComponent,
-  },
-  {
-    path: 'products',
-    component: ProductsComponent,
-  },
-  {
-    path: 'products/:id', // envio de parametro
-    component: ProductDetailComponent,
-  },
-  {
-    path: 'contact',
-    component: ContactComponent,
+    component: LayoutComponent,
+    children: [
+      {
+        path: '',
+        redirectTo: '/home',
+        pathMatch: 'full',
+        component: HomeComponent,
+      },
+      {
+        path: 'products',
+        component: ProductsComponent,
+      },
+      {
+        path: 'products/:id', // envio de parametro
+        component: ProductDetailComponent,
+      },
+      {
+        path: 'contact',
+        component: ContactComponent,
+      },
+    ]
   },
   {
     path: 'demo',
