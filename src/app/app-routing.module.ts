988ef28ff14d1c93b011ddf74f01c3b9ff9989b3
Dennis Ake <dariel_504@hotmail.com>
2021-03-05 21:27:18 -0600

    preparación e implementación del Lazy Loading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,7 @@
 import { Component, NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { PreloadAllModules, PreloadingStrategy, RouterModule, Routes } from '@angular/router';
 import { ContactComponent } from './contact/contact.component';
 
-import { HomeComponent } from './home/home.component';
 import { ProductsComponent } from './products/products.component';
 import { DemoComponent } from './demo/demo.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
@@ -20,7 +19,7 @@ const routes: Routes = [
       },
       {
         path: 'home',
-        component: HomeComponent,
+        loadChildren: () => import('./home/home.module').then(m => m.HomeModule)
       },
       {
         path: 'products',
@@ -47,7 +46,9 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {
+    preloadingStrategy: PreloadAllModules
+  })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
