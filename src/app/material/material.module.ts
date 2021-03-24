f7748da1bb4ff4e3392ccb22f9ffb4aecfac9d9c
Dennis Ake <dariel_504@hotmail.com>
2021-03-24 00:35:54 -0600

    Estilos a product-card

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -3,16 +3,19 @@ import { CommonModule } from '@angular/common';
 
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatCardModule } from "@angular/material/card";
 @NgModule({
   declarations: [],
   imports: [
     CommonModule,
     MatButtonModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatCardModule
   ],
   exports: [
     MatButtonModule,
-    MatToolbarModule
+    MatToolbarModule,
+    MatCardModule
   ]
 })
 export class MaterialModule { }
