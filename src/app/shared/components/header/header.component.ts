1625d5d38e0b63777f8565258ce0e8abd05595c4
Dennis Ake <dariel_504@hotmail.com>
2021-04-14 12:20:34 -0500

    uso de pipes para contador de productos

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { map } from "rxjs/operators";
 import { CartService } from "./../../../core/services/cart.service";
+
+
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
@@ -7,15 +11,15 @@ import { CartService } from "./../../../core/services/cart.service";
 })
 export class HeaderComponent implements OnInit {
 
-  total = 0;
+  total$ = new Observable<number>();
 
   constructor(
     private cartService: CartService
   ) {
-    this.cartService.cart$.subscribe(products => {
-      console.log(products);
-      this.total = products.length;
-    });
+    this.total$ =this.cartService.cart$
+      .pipe(
+        map(products => products.length)
+      );
   }
 
   ngOnInit(): void {
