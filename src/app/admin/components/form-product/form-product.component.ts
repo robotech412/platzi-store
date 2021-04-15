47d6cffc35939a276cc99f0180fdb8af26271f62
Dennis Ake <dariel_504@hotmail.com>
2021-04-15 00:54:46 -0500

    practica subir imagena  Firebase Storage

--- src/app/admin/components/form-product/form-product.component.ts
+++ src/app/admin/components/form-product/form-product.component.ts
@@ -1,9 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validator, Validators } from "@angular/forms";
 import { Router } from "@angular/router";
+import { AngularFireStorage } from "@angular/fire/storage";
+
+import { finalize } from "rxjs/operators";
+
 import { MyValidators } from "./../../../utils/validators";
 
 import { ProductsService } from "./../../../core/services/products/products.service";
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-form-product',
@@ -13,11 +18,13 @@ import { ProductsService } from "./../../../core/services/products/products.serv
 export class FormProductComponent implements OnInit {
 
   form: FormGroup;
+  image$: Observable<any>;
 
   constructor(
     private FormBuilder: FormBuilder,
     private productService: ProductsService,
-    private router: Router
+    private router: Router,
+    private storage: AngularFireStorage
   ) {
     this.buildForm();
   }
@@ -38,6 +45,26 @@ export class FormProductComponent implements OnInit {
     console.log(this.form.value);
   }
 
+  uploadFile(event) {
+    const file = event.target.files[0];
+    const name = 'images';
+    const fileRef = this.storage.ref(name);
+    const task = this.storage.upload(name, file);
+
+    task.snapshotChanges()
+      .pipe(
+        finalize(() => {
+          this.image$ = fileRef.getDownloadURL();
+          this.image$.subscribe(url=>{
+            console.log(url);
+            this.form.get('image').setValue(url);
+          })
+        })
+      )
+      .subscribe();
+  }
+
+
   private buildForm() {
     this.form = this.FormBuilder.group({
       id: ['', [Validators.required]],
@@ -48,8 +75,8 @@ export class FormProductComponent implements OnInit {
     });
   }
 
-get priceField(){
-  return this.form.get('price');
-}
+  get priceField() {
+    return this.form.get('price');
+  }
 
 }
